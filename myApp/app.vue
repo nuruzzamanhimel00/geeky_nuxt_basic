fe18c41ada5ea0329460a73684009d4fd3b2d373
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-09-09 22:46:16 +0600

    init

---
+++ myApp/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>This is Global Component</h1>
+    <NuxtPage />
+  </div>
+</template>
---
+++ myApp/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Home page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam vitae
+      voluptatum a minus perferendis consequatur praesentium rerum, natus
+      incidunt, corrupti similique aperiam quaerat impedit, officia quis. Vitae
+      qui ea rem.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
