84e4b978e202e07e1d91bd777d204eb2134d385a
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-09-13 22:06:31 +0600

    done

---
+++ ch9/pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>About pages</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+      architecto deserunt ut odit facere provident itaque eligendi illum nisi
+      amet? Impedit cum iure corrupti alias earum voluptatum soluta quo cumque.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ ch9/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Home pages</h1>
+    <Navbar />
+    <hr />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+      architecto deserunt ut odit facere provident itaque eligendi illum nisi
+      amet? Impedit cum iure corrupti alias earum voluptatum soluta quo cumque.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
