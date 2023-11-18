5cb91aea4f4f13774f57d1e42c2bd1909451adee
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-11-19 00:09:27 +0600

    done

---
+++ com_api_1/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="nav">
+    <li class="nav-item">
+      <NuxtLink class="nav-link active" to="/">Home</NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" :to="{ name: 'tut_102' }">Tut 102</NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" :to="{ name: 'tut_103' }">Tut 103</NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" :to="{ name: 'tut_104' }">Tut 104</NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" :to="{ name: 'tut_105' }">Tut 105</NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" :to="{ name: 'tut_106' }">Tut 106</NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" :to="{ name: 'tut_108' }">Tut 108</NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" :to="{ name: 'tut_109' }">Tut 109</NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" :to="{ name: 'tut_110' }">Tut 110</NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
---
+++ com_api_1/components/UserDetails.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div>
+      <hr />
+      <h1>User Detaisl component---------------</h1>
+      <h4>Name: {{ user.name }}</h4>
+      <h4>age: {{ user.age }}</h4>
+      <button @click="changeDetials">Change detials</button>
+    </div>
+    <hr />
+    <div>
+      <input type="text" ref="ageOf" />
+      <button @click.prevent="changeAgeHandler">Change age Template Ref</button>
+    </div>
+    <br />
+    <button @click.prevent="onChangeFNHandler">
+      Change First name Custom event
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserDetails",
+  props: ["userDetails"],
+  setup(props, context) {
+    const user = reactive(props.userDetails);
+    const ageOf = ref(null);
+    function changeDetials() {
+      user.name = "change name XXXX";
+      user.age = "change age 20";
+    }
+
+    function changeAgeHandler() {
+      user.age = ageOf.value.value;
+      console.log(ageOf.value.value);
+    }
+
+    function onChangeFNHandler() {
+      context.emit("first-name-change", "first name modified...");
+    }
+
+    return {
+      user,
+      changeDetials,
+      changeAgeHandler,
+      ageOf,
+      onChangeFNHandler,
+    };
+  },
+};
+</script>
+<!-- <script setup></script> -->
+
+<style lang="scss" scoped></style>
